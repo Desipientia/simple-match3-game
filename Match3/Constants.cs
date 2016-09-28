28ce783a24b601d08bd774fba75b8da126aaf432
Desipientia <daria.shonova@gmail.com>
2016-09-28 19:32:20 +1000

    Set all current constant values

---
+++ Match3/Constants.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Match3
+{
+    static class Constants
+    {
+        public const int BufferWidth = 720;
+        public const int BufferHeight = 720;
+
+        public const int GameDuration = 3;
+
+        public const string GameName = "Match3 Balls";
+        public const string BaseFontName = "base-font";
+
+        public const string MainMenuTexture = "img/main-menu";
+
+        public const int PlayButtonWidth = 290;
+        public const int PlayButtonHeight = 120;
+        public const int PlayButtonX = 220;
+        public const int PlayButtonY = 340;
+        public const string PlayButtonTexture = "img/play-btn";
+        public const string PlayButtonHoverTexture = "img/play-btn-hover";
+        public const string PlayButtonPressTexture = "img/play-btn-press";
+
+        public const int ScoreBoxX = 20;
+        public const int TimeBoxX = 540;
+        public const int ScoreBoardY = 30;
+        public const int GameFieldX = 120;
+        public const int GameFieldY = 170;
+
+        public const string GameScreenTexture = "img/game-screen";
+        public const string GameOverMessageTexture = "img/game-over-message";
+
+        public const int OkButtonWidth = 200;
+        public const int OkButtonHeight = 100;
+        public const int OkButtonX = 260;
+        public const int OkButtonY = 330;
+        public const string OkButtonTexture = "img/ok-btn";
+        public const string OkButtonHoverTexture = "img/ok-btn-hover";
+        public const string OkButtonPressTexture = "img/ok-btn-press";
+    }
+}
